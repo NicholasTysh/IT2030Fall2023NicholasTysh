07628d8e6ad253aec95515d9e997356219393c34
NicholasTysh <s01300805@acad.tri-c.edu>
2023-10-01 20:47:02 -0400

    update readme

---
+++ Week-5/NickPixPlus/Data/NickPixPlusContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NickPixPlus.Models;
+
+namespace NickPixPlus.Data
+{
+    public class NickPixPlusContext : DbContext
+    {
+        public NickPixPlusContext (DbContextOptions<NickPixPlusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NickPixPlus.Models.Movie> Movie { get; set; } = default!;
+
+        public DbSet<NickPixPlus.Models.ContactModel>? ContactModel { get; set; }
+    }
+}
---
+++ Week-5/NickPixPlus/Models/ContactModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NickPixPlus.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid first name as a string.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid last name as a string.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid address as a string.")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid phone number as a string.")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid email address as a string.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Must enter valid message as a string.")]
+        public string? Message { get; set; }
+    }
+}
