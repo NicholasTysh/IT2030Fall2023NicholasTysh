eea36f57b3404541cfa52e1cbf7e8f4d94b3db1a
nicholastysh <s01300805@acad.tri-c.edu>
2023-10-15 16:35:16 -0400

    push week 7

---
+++ Week-7/NickPixPlus/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NickPixPlus.Models;
+namespace NickPixPlus.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Week-7/NickPixPlus/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.X86;
+
+namespace NickPixPlus.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; } = string.Empty;
+        public string? ProductDescription { get; set; } = string.Empty;
+        public string? ProductImage { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+    }
+}
